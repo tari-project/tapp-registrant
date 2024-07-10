e318e1a7214e12ae4e3d064f7425b2f223f7796e
karczuRF <oskar.karcz@rumblefish.pl>
2024-07-10 12:11:22 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -54,35 +54,3 @@ program
   })
 
 program.parse()
-
-// program
-//   .version("1.0.11")
-//   .description("The tapplet registration tool")
-//   .option("-i, --init", "create manifest file")
-//   .option("-r, --register", "register the tapplet")
-//   .option("-u, --update", "ipdate tapplet version")
-//   .option("-d, --deprecate <VERSION>", "deprecate the given tapplet version")
-//   .option("-l, --list", "list registered tapplets")
-//   .option("-g, --ghp", "find and add GitHub Access Token")
-//   .parse(process.argv)
-
-// const options = program.opts()
-
-// if (options.init) {
-//   createManifest()
-// }
-// if (options.list) {
-//   listRegisteredTapplets()
-// }
-// if (options.update) {
-//   updateTappVersion()
-// }
-// if (options.register) {
-//   registerTapp()
-// }
-// if (options.deprecate) {
-//   deprecateTappVersion(options.deprecate)
-// }
-// if (options.ghp) {
-//   getGhp()
-// }
